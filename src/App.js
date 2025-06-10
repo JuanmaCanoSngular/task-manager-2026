ac84e100959e2bbdafe12dd4fabccad8a601432f
JuanmaCano1980 <juanmanuel.cano@sngular.com>
2025-06-10 15:28:50 +0200

    eliminar config. innecesaria

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from 'react/jsx-runtime';
-var App = function () {
-    return _jsx('div', { children: 'Hello World' });
-};
-export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-    return <div>Hello World</div>;
+    return (
+        <div className="h-screen w-full flex items-center justify-center bg-gray-100">
+            <h1 className="text-3xl font-bold text-blue-600">Hello World</h1>
+        </div>
+    );
 };
 
 export default App;
